99febf3c5bb14220b46afe63d6cd690772c21a6a
songkungsun <thdrudtjssla1234@gmail.com>
2024-04-12 19:40:37 +0900

    ADD All Filter Combined Code

---
+++ code/log_main.py
@@ -0,0 +1,10 @@
+import os
+
+# log_config.py 실행
+os.system("python3 log_config.py")
+
+# datatransform.py 실행
+os.system("python3 log_transform.py")
+
+# datatransport.py 실행
+os.system("python3 log_transport.py") 
