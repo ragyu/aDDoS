366e465a85bcf141e71130ce7e8a1d97d2be57d2
ragyu <ragyu0506@naver.com>
2024-06-09 19:04:07 +0900

    UPDATE MyInfo TSX

--- code/frontend/src/app/components/Sidebar/contents/MyInfo.tsx
+++ code/frontend/src/app/components/Sidebar/contents/MyInfo.tsx
@@ -1,168 +1,92 @@
+'use client';
 import React, { useState } from 'react';
-import styles from './Traffic.module.css';
+import styles from './Myinfo.module.css';
+import Image from 'next/image';
 
-const Graph = () => {
-  const [timeInterval, setTimeInterval] = useState(5); // 시간 간격 (기본값: 5분)
-  const [duration, setDuration] = useState(60); // 지속 시간 (기본값: 60분)
+function Myinfo() {
+  const [image, setImage] = useState('/assets/user.png');
+  const [introduction, setIntroduction] =
+    useState('여기에 사용자 소개를 적어주세요.');
+  const [tempIntroduction, setTempIntroduction] = useState('');
 
-  const handleChangeTimeInterval = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const inputValue = Number(event.target.value);
-    const newValue = Math.min(Math.max(inputValue, 1), 10080);
-    setTimeInterval(newValue);
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        const result = e.target?.result as string;
+        setImage(result);
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
   };
 
-  const handleChangeDuration = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = Number(event.target.value);
-    const newValue = Math.min(Math.max(inputValue, 1), 10080);
-    setDuration(newValue);
+  const handleSubmitIntroduction = () => {
+    setIntroduction(tempIntroduction);
   };
-
-  const endDate = new Date();
-  endDate.setMinutes(endDate.getMinutes(), 0, 0); // 현재 분을 기준으로 정각으로 설정
-
-  const formatTime = (date: Date) => {
-    const hours = date.getHours();
-    const minutes =
-      date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-    return `${hours}:${minutes}`;
-  };
-
-  // 유효한 배열 길이를 보장하기 위해 Math.max를 사용하여 0보다 작은 값을 방지
-  const maxPoints = 49;
-  const points = Math.max(Math.ceil(duration / timeInterval), 0);
-  const timesLength = Math.min(points, maxPoints);
-  const times = Array.from({ length: timesLength }, (_, i) =>
-    formatTime(new Date(endDate.getTime() - timeInterval * 60000 * i))
-  );
-
-  // 랜덤 데이터 생성
-  const generateRandomDataRed = () => {
-    return Array.from({ length: timesLength }, () =>
-      Math.floor(Math.random() * 101)
-    );
-  };
-
-  const generateRandomDataBlue = () => {
-    return Array.from({ length: timesLength }, () =>
-      Math.floor(Math.random() * 101)
-    );
-  };
-
-  const randomDataRed = generateRandomDataRed();
-  const randomDataBlue = generateRandomDataBlue();
-
-  const a = times.length;
-  const b = times.length > 1 ? 300 / (times.length - 1) : 0;
-  const c = (a - 1) * b;
-
-  const d = 6;
-  const e = 20;
-  const f = (d - 1) * e;
 
   return (
-    <div>
-      <div className={styles.graph}>
-        <h1>내정보</h1>
-        <svg viewBox="-110 -10 500 130" style={{ fontSize: '5px' }}>
-          {Array.from({ length: d }, (_, i) => (
-            <React.Fragment key={`hline-${i}`}>
-              <line
-                x1="0"
-                y1={100 - e * i}
-                x2={c}
-                y2={100 - e * i}
-                stroke="#ccc"
-                strokeWidth="0.3"
-              />
-              <text x="-5" y={102 - e * i} textAnchor="end">
-                {0 + 20 * i}
-              </text>
-            </React.Fragment>
-          ))}
-          {Array.from({ length: a }, (_, i) => (
-            <>
-              <line
-                key={`vline-${i}`}
-                x1={b * i}
-                y1={100 - f}
-                x2={b * i}
-                y2={100}
-                stroke="#ccc"
-                strokeWidth="0.3"
-              />
-              {i < a - 1 && (
-                <line
-                  key={`line-${i}`}
-                  x1={b * i}
-                  y1={100 - randomDataRed[i]}
-                  x2={b * (i + 1)}
-                  y2={100 - randomDataRed[i + 1]}
-                  stroke="red"
-                  strokeWidth="0.8"
-                />
-              )}
-              <circle
-                cx={b * i}
-                cy={100 - randomDataRed[i]} // 랜덤 데이터 값을 y 좌표로 사용
-                r="1.1"
-                fill="red"
-              />
-              {i < a - 1 && (
-                <line
-                  key={`line-${i}`}
-                  x1={b * i}
-                  y1={100 - randomDataBlue[i]}
-                  x2={b * (i + 1)}
-                  y2={100 - randomDataBlue[i + 1]}
-                  stroke="blue"
-                  strokeWidth="0.8"
-                />
-              )}
-              <circle
-                cx={b * i}
-                cy={100 - randomDataBlue[i]} // 랜덤 데이터 값을 y 좌표로 사용
-                r="1.1"
-                fill="blue"
-              />
-            </>
-          ))}
-          {times.reverse().map((time, i) => (
-            <text
-              key={`time-${i}`}
-              x={b * i}
-              y="110"
-              textAnchor="middle"
-              transform={b < 15 ? `rotate(-45, ${b * i}, 110)` : ''}
-            >
-              {time}
-            </text>
-          ))}
-        </svg>
-      </div>
-      <div className={styles.label}>
-        <label>
-          시간 간격 (분):
+    <div className={styles.container}>
+      <div className={styles.profileSection}>
+        <div className={styles.avatarContainer}>
+          <Image
+            src={image}
+            alt="프로필사진"
+            className={styles.avatar}
+            width={80}
+            height={80}
+          />
+        </div>
+        <label htmlFor="imageInput" className={styles.changeImageButton}>
+          이미지 변경
           <input
-            type="number"
-            value={timeInterval}
-            onChange={handleChangeTimeInterval}
+            id="imageInput"
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            style={{ display: 'none' }}
           />
         </label>
-        &nbsp;
-        <label>
-          표기 시간 (분):
-          <input
-            type="number"
-            value={duration}
-            onChange={handleChangeDuration}
-          />
-        </label>
-        {/* {b} */}
       </div>
+
+      <table className={styles.infoSection}>
+        <tr>
+          <td className={styles.label}>이메일</td>
+          <td className={styles.data}>user@example.com</td>
+        </tr>
+        <tr>
+          <td className={styles.label}>이름</td>
+          <td className={styles.data}>UserName</td>
+        </tr>
+        <tr>
+          <td className={styles.label}>성별</td>{' '}
+          <td className={styles.data}>남성</td>
+        </tr>
+        <tr>
+          <td className={styles.label}>전화번호</td>{' '}
+          <td className={styles.data}>010-1234-5678</td>
+        </tr>
+        <tr>
+          <td className={styles.label}>가입날짜</td>{' '}
+          <td className={styles.data}>2024-04-30</td>
+        </tr>
+        <tr>
+          <td className={styles.label}>소개</td>
+          <td className={styles.data}>{introduction}</td>
+        </tr>
+      </table>
+      <textarea
+        className={styles.introductionInput}
+        placeholder="소개를 수정하세요."
+        value={tempIntroduction}
+        onChange={(e) => setTempIntroduction(e.target.value)}
+      />
+      <button className={styles.edit} onClick={handleSubmitIntroduction}>
+        수정하기
+      </button>
     </div>
   );
-};
+}
 
-export default Graph;
+export default Myinfo;
