105c1516182ec1c3610c755800efeb61d7b915ed
ragyu <ragyu0506@naver.com>
2024-06-09 19:36:30 +0900

    ADD mypage TSX

--- code/frontend/src/app/mypage/page.tsx
+++ code/frontend/src/app/mypage/page.tsx
@@ -1,13 +1,10 @@
 import styles from './mypage.module.css';
 import Sidebar from '../components/Sidebar/Sidebar';
-const MyPage: React.FC = () => {
-  const data = [0, 59, 50, 81, 56, 55, -50]; // 그래프에 표시할 데이터
 
+export default function MyPage() {
   return (
     <div>
       <Sidebar />
     </div>
   );
-};
-
-export default MyPage;
+}
