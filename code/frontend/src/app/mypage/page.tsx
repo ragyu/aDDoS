764a202afabf95438bea9e65d975673ec8287f13
ragyu <ragyu0506@naver.com>
2024-06-17 22:05:11 +0900

    ADD faq TSX

--- code/frontend/src/app/mypage/page.tsx
+++ code/frontend/src/app/mypage/page.tsx
@@ -1,7 +1,7 @@
 import styles from './mypage.module.css';
 import Sidebar from '../components/Sidebar/Sidebar';
 
-export default function MyPage() {
+export default function mypage() {
   return (
     <div>
       <Sidebar />
