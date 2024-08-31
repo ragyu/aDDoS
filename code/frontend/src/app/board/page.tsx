8938f19c6c0418cfee6c492b3d239269422121c9
ragyu <ragyu0506@naver.com>
2024-08-31 23:04:39 +0900

    ADD board TSX

--- code/frontend/src/app/board/page.tsx
+++ code/frontend/src/app/board/page.tsx
@@ -14,42 +14,42 @@ const posts: Post[] = [
     id: 1,
     title: 'ㅎㅇㅎㅇ님들',
     author: '김동철',
-    createdAt: '2024-05-01',
+    createdAt: '2024-06-01',
     likes: 20,
   },
   {
     id: 2,
     title: 'ㅎㅇㅎㅇ님들',
     author: '박주형',
-    createdAt: '2024-05-02',
+    createdAt: '2024-06-12',
     likes: 56,
   },
   {
     id: 3,
     title: 'ㅎㅇㅎㅇ님들',
     author: '송경선',
-    createdAt: '2024-05-05',
+    createdAt: '2024-06-25',
     likes: 93,
   },
   {
     id: 4,
     title: 'ㅎㅇㅎㅇ님들',
     author: '이건우',
-    createdAt: '2024-05-09',
+    createdAt: '2024-07-09',
     likes: 37,
   },
   {
     id: 5,
     title: 'ㅎㅇㅎㅇ님들',
     author: '이두리',
-    createdAt: '2024-05-17',
+    createdAt: '2024-07-17',
     likes: 68,
   },
   {
     id: 6,
     title: 'ㅎㅇㅎㅇ님들',
     author: '이라규',
-    createdAt: '2024-05-23',
+    createdAt: '2024-07-23',
     likes: 71,
   },
 ];
@@ -57,17 +57,16 @@ const posts: Post[] = [
 const Board = () => {
   return (
     <div className={styles.board}>
-      <div className={styles.boardtext}>자유게시판</div>
+      <div className={styles.boardtext}>공지사항</div>
       {posts.map((post) => (
         <li key={post.id} className={styles.postItem}>
           <span className={styles.idRatio}>{post.id} </span>
           <span className={styles.titleRatio}>{post.title}</span>
           <span className={styles.authorRatio}>{post.author} </span>
           <span className={styles.createdAtRatio}>{post.createdAt} </span>
-          <span className={styles.likesRatio}>좋아요: {post.likes}</span>
         </li>
       ))}
-      <Button type="submit" text="글쓰기" />
+      {/* <Button type="submit" text="글쓰기" /> */}
     </div>
   );
 };
