aba72ec1bfffc03612a999284210752bf84a3416
ragyu <ragyu0506@naver.com>
2024-08-31 23:10:05 +0900

    ADD notice TSX

--- code/frontend/src/app/notice/page.tsx
+++ code/frontend/src/app/notice/page.tsx
@@ -1,7 +1,26 @@
 import styles from './notice.module.css';
-import Link from 'next/link';
+import Post from '../components/Post/Post';
 
 export default function notice() {
+  const notices = [
+    {
+      id: 1,
+      title: '공지사항 예시 제목',
+      author: '관리자',
+      date: '2024-08-01',
+    },
+    { id: 2, title: '안녕하세요', author: 'Admin', date: '2024-08-08' },
+    {
+      id: 3,
+      title:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, iure vel veniam pariat',
+      author: 'GM',
+      date: '2024-08-13',
+    },
+    { id: 4, title: '반갑습니다', author: 'Master', date: '2024-08-15' },
+    { id: 5, title: '공지사항입니다.', author: 'King', date: '2024-08-26' },
+  ];
+
   return (
     <div className={styles.noticeWrap}>
       <div className={styles.notice}>
@@ -13,51 +32,15 @@ export default function notice() {
           <span className={styles.infoItem}>작성일</span>
         </div>
 
-        <div className={styles.infoBottom}>
-          <span className={styles.infoData}>1</span>
-          <Link href="#" className={styles.infoData}>
-            공지사항 예시 제목
-          </Link>
-          <span className={styles.infoData}>관리자</span>
-          <span className={styles.infoData}>2024-05-03</span>
-        </div>
-
-        <div className={styles.infoBottom}>
-          <span className={styles.infoData}>2</span>
-          <Link href="#" className={styles.infoData}>
-            ㅎㅇㅎㅇ
-          </Link>
-          <span className={styles.infoData}>Admin</span>
-          <span className={styles.infoData}>2024-05-03</span>
-        </div>
-
-        <div className={styles.infoBottom}>
-          <span className={styles.infoData}>3</span>
-          <Link href="#" className={styles.infoData}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Perferendis, iure vel veniam pariat
-          </Link>
-          <span className={styles.infoData}>GM</span>
-          <span className={styles.infoData}>2024-05-03</span>
-        </div>
-
-        <div className={styles.infoBottom}>
-          <span className={styles.infoData}>4</span>
-          <Link href="#" className={styles.infoData}>
-            호우
-          </Link>
-          <span className={styles.infoData}>Master</span>
-          <span className={styles.infoData}>2024-05-03</span>
-        </div>
-
-        <div className={styles.infoBottom}>
-          <span className={styles.infoData}>5</span>
-          <Link href="#" className={styles.infoData}>
-            헤헤
-          </Link>
-          <span className={styles.infoData}>King</span>
-          <span className={styles.infoData}>2024-05-03</span>
-        </div>
+        {notices.map((notice) => (
+          <Post
+            key={notice.id}
+            id={notice.id}
+            title={notice.title}
+            author={notice.author}
+            date={notice.date}
+          />
+        ))}
       </div>
     </div>
   );
